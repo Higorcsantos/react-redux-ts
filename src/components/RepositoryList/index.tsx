a6c352cc1fb91cec007c2a54d990d718f2240b47
Higor Cerqueira dos Santos <higorcsantos18@gmail.com>
2021-03-21 19:41:16 -0300

    Add reducers to component

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,12 +1,28 @@
+import { type } from 'node:os';
 import React,{Component} from 'react';
+import {connect} from 'react-redux'
+import {ApplicationState} from '../../store';
 
+interface StateProps{
 
-export default class RepositoryList extends Component{
+};
+interface DispatchProps{
+
+};
+interface OwnProps{
+
+};
+
+type props = StateProps & DispatchProps & OwnProps
+ 
+class RepositoryList extends Component{
     render(){
         return(
-            <div>
-
-            </div>
+            <div/>
         )
     }
 }
+const mapStateToProps = (state:ApplicationState) => ({
+    repositories: state.repositories.data
+})
+export default connect(mapStateToProps)(RepositoryList);
