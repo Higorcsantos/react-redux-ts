dce261015c1885861b40b5124b37e2308c9a5172
Higor Cerqueira dos Santos <higorcsantos18@gmail.com>
2021-03-21 19:21:15 -0300

    reducer config

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,23 +1,8 @@
 import React,{Component} from 'react';
 
-interface Repository {
-    id: number,
-    name: string
-};
-interface Props {
-    repositories: Repository[];
-};
-interface State{
-    newRepository ?: string
-}
-export default class RepositoryList extends Component<Props,State>{
-    state = {
-        newRepository: '',
-    }
+
+export default class RepositoryList extends Component{
     render(){
-        const {repositories} = this.props;
-        const {newRepository} = this.state;
-        
         return(
             <div>
 
---
+++ src/store/ducks/repositories/actions.ts
@@ -0,0 +1,8 @@
+import {action} from 'typesafe-actions';
+import {RepositoriesTypes,Repository} from './types';
+
+export const loadRequest = () => action(RepositoriesTypes.LOAD_REQUEST);
+
+export const loadFailure = () => action(RepositoriesTypes.LOAD_FAILURE);
+
+export const loadSucces = (data: Repository) => (RepositoriesTypes.LOAD_SUCCESS, data);
