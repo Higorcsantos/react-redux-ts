ab21bbfe206149f2c329d60f2535ad7f05faf689
Higor Cerqueira dos Santos <higorcsantos18@gmail.com>
2021-03-22 15:34:23 -0300

    sagas finished

--- src/store/ducks/repositories/sagas.ts
+++ src/store/ducks/repositories/sagas.ts
@@ -1,11 +1,14 @@
-import {call,put} from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 import api from '../../../services/api';
-import {loadSucces,loadFailure} from './actions'
 
-export function* load(){
-    try{
-        
-    }catch(err){
+import { loadSuccess, loadFailure } from './actions';
 
-    }
+export function* load() {
+  try {
+    const response = yield call(api.get, 'users/diego3g/repos');
+
+    yield put(loadSuccess(response.data));
+  } catch (err) {
+    yield put(loadFailure());
+  }
 }
